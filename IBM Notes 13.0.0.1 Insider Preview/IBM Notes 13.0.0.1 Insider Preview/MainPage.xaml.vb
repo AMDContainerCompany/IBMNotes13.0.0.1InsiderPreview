437e5169e1baf16bde9047ecda34e016e2cebae3
NataliaVodianova <39686420+NataliaVodianova@users.noreply.github.com>
2018-05-29 06:29:57 +0900

    Internet DO
    HHHll--

---
+++ IBM Notes 13.0.0.1 Insider Preview/IBM Notes 13.0.0.1 Insider Preview/MainPage.xaml.vb
@@ -0,0 +1,8 @@
+﻿Dim log As New EventLog("MyApplication1", "IBM i", "Notes-Dev")
+log.WriteEntry("19.8", EventLogEntryType.Warning, 502)
+
+Dim log As New EventLog("MyApplication1", "IBM i", "Notes-Dev")
+log.WriteEntry("19.8.2", EventLogEntryType.Warning, 502)
+
+Dim log As New EventLog("MyApplication1", "IBM i", "Notes-Dev")
+log.WriteEntry("19.8.6", EventLogEntryType.Warning, 502)
